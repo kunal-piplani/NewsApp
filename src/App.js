e2767862cdb7088557ae272ac638e756114fd816
kunal piplani <kunal.piplani12@gmail.com>
2021-09-03 12:06:29 +0530

    V22-NewsAPP

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react'
+
+
+   
+    
+    export default class App extends Component {
+      c ="KP"
+      render() {
+       
+        return (
+          <div>
+       TEST --{this.c}
+          </div>
+        )
+      }
+    }
+    
+
