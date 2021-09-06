3e375826539c21222c3d0298619fea5affe5c69b
kunal piplani <kunal.piplani12@gmail.com>
2021-09-06 13:13:28 +0530

    NewsAPP-V28

--- src/components/News.js
+++ src/components/News.js
@@ -8,20 +8,54 @@ articles =  []
         super();
         console.log("Constr from News Component ");
            this.state={
-            articles:[]
-
-           }
+            articles:[],
+            page :1,
+        
+        }
     
     }
 async componentDidMount(){
 
 
-    let url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2bfde93e3f87449991a5d025e5c4b65e";
+    let url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2bfde93e3f87449991a5d025e5c4b65e&page=1&pageSize=21";
     let data =await fetch(url);
     let parseddata =await data.json();
     console.log(parseddata);
-    this.setState({articles:parseddata.articles});
+    this.setState({articles:parseddata.articles,
+        totalResults:parseddata.totalResults
+    });
 }
+handlePreviousButton=async()=>{
+
+    let url =`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2bfde93e3f87449991a5d025e5c4b65e&page=${this.state.page -1}&pageSize=21`;
+    let data =await fetch(url);
+    let parseddata =await data.json();
+this.setState({
+page:this.state.page-1,
+articles:parseddata.articles
+})
+
+
+
+}
+handleNextButton=async()=>{
+    
+    if(this.state.page+1>Math.ceil(this.state.totalResults/21)){
+
+
+    }
+    else{
+    let url =`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2bfde93e3f87449991a5d025e5c4b65e&page=${this.state.page +1}&pageSize=21`;
+    let data =await fetch(url);
+    let parseddata =await data.json();
+this.setState({
+page:this.state.page+1,
+articles:parseddata.articles
+})
+
+}
+}
+
   render() {
     return (
       <div className="container my-3">
@@ -34,7 +68,10 @@ async componentDidMount(){
               
               </div> 
             })}
-          
+            </div>
+          <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-success" onClick={this.handlePreviousButton}>&laquo; previous</button>
+          <button type="button" className="btn btn-success" onClick={this.handleNextButton}>next &raquo;</button>
           </div>
 
          
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,8 +12,8 @@ export class NewsItem extends Component {
   <img src={imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Details View</a>
+    <p className="card-text">{description}</p>
+    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Details View</a>
   </div>
 </div>
             </div>
